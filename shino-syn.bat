8fb7a2ebcc6c8c890e39387af3cd5128a24cb307
alpaca-honke <blacktea1220@hotmail.com>
2022-11-24 21:16:40 +0900

    update shino-syn shino-syn.bat shino-syn.ps1

--- shino-syn.bat
+++ shino-syn.bat
@@ -2,11 +2,11 @@
 if "%1" EQU "--help" (
     echo 単語の読み方を表示します。
     echo 用法
-    echo >東雲構文 調べる単語
+    echo >shino-syn 調べる単語
 ) else if "%1" EQU "-h" (
     echo 単語の読み方を表示します。
     echo 用法
-    echo >東雲構文 調べる単語
+    echo >shino-syn 調べる単語
 ) else if "%1" EQU "" (
     echo 引数が空です。-hもしくは--helpで使い方を表示
 ) else (
--- shino-syn.ps1
+++ shino-syn.ps1
@@ -4,7 +4,7 @@ Param( $word )
 if ( $word -in @("-h","--help")){
     Write-Host 単語の読み方を表示します。
     Write-Host 用法
-    Write-Host ">東雲構文 調べる単語"
+    Write-Host ">.\shino-syn.ps1 調べる単語"
 } elseif ( $word -in @("", $null)){
     Write-Host 引数が空です。-hもしくは--helpで使い方を表示
 } else {
