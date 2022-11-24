5618cf93bc2c8afe086be8fafffd1ede543d4b16
alpaca-honke <blacktea1220@hotmail.com>
2022-11-24 16:56:28 +0900

    add shellscript(dash) and powershell

---
+++ 東雲構文.ps1
@@ -0,0 +1,17 @@
+#!/usr/bin/env pwsh
+Param( $word )
+
+if ( $word -in @("-h","--help")){
+    Write-Host 単語の読み方を表示します。
+    Write-Host 用法
+    Write-Host ">東雲構文 調べる単語"
+} elseif ( $word -in @("", $null)){
+    Write-Host 引数が空です。-hもしくは--helpで使い方を表示
+} else {
+    Write-Host "$word" -NoNewLine
+    Write-Host "の読み方知らなくてずっと" -NoNewLine
+    Write-Host "$word" -NoNewLine
+    Write-Host "って読んでた"
+    Write-Host "$word" -NoNewLine
+    Write-Host "って読むのか"
+}
